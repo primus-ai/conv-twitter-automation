6fcc62f10fd4efca7a6c15f0ef6c6340c2d316f2
Primus AI <47700844+primus-ai@users.noreply.github.com>
2022-05-27 18:50:15 -0400

    Update tweets_collection.R

--- tweets_collection.R
+++ tweets_collection.R
@@ -8,7 +8,7 @@ library(tidyverse)
 # constituyentes <- constituyentes %>% filter(screen_name != "convencioncl")
 # saveRDS(constituyentes, file = "data/constituyentes.rds")
 rbot_token <- rtweet::create_token(
-  app = "twitternetwork2",
+  app = "rtweet-demo-primus",
   # the name of the Twitter app
   consumer_key = Sys.getenv("RBOT_TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("RBOT_TWITTER_CONSUMER_API_SECRET"),
