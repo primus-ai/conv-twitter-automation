69d9f3bd955820c2ec0fec995e77b5e68501b9f1
Primus AI <47700844+primus-ai@users.noreply.github.com>
2022-05-27 15:44:22 -0400

    Update tweets_collection.R
    Add twitter token

--- tweets_collection.R
+++ tweets_collection.R
@@ -7,6 +7,16 @@ library(tidyverse)
 # constituyentes <- lists_members(list_id = "1395879401858994179")
 # constituyentes <- constituyentes %>% filter(screen_name != "convencioncl")
 # saveRDS(constituyentes, file = "data/constituyentes.rds")
+rbot_token <- rtweet::create_token(
+  app = "twitternetwork2",
+  # the name of the Twitter app
+  consumer_key = Sys.getenv("RBOT_TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("RBOT_TWITTER_CONSUMER_API_SECRET"),
+  access_token = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("RBOT_TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
+)
+
 
 constituyentes <- readRDS(file = "data/constituyentes.rds")
  
